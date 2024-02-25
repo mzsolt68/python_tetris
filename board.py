fb7b0496862f7ee474b0f2eda4b8a5ae504a19c8
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2024-02-25 14:51:29 +0100

    Move update function to the class

--- board.py
+++ board.py
@@ -1,8 +1,17 @@
 ''' This module contains the GameBoard class. '''
 
+from piece import Piece
+
 class GameBoard:
     ''' A class to represent the game board. '''
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.board = [['.' for _ in range(self.width)] for _ in range(self.height)]
+
+    def update(self, piece: Piece):
+        ''' Update the board with the piece. '''
+        for row in range(5):
+            for column in range(5):
+                if piece.shape[piece.rotation][row][column] == 'x':
+                    self.board[piece.row + row][piece.column + column] = 'x'
